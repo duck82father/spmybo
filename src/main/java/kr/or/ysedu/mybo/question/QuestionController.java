2e59e7fcde8c3a8a730ec12ea476a75d16d29509
duck82father <legnaevol84@gmail.com>
2024-01-03 16:04:30 +0900

    02 질문목록

---
+++ src/main/java/kr/or/ysedu/mybo/question/QuestionController.java
@@ -0,0 +1,30 @@
+package kr.or.ysedu.mybo.question;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+
+
+@RequiredArgsConstructor
+@Controller
+public class QuestionController {
+	
+	private final QuestionRepository questionRepository;
+
+//  '@RequiredArgsConstructor'가 담당하는 역할 
+//	public QuestionController (QuestionRepository questionRepository) {
+//		this.questionRepository = questionRepository;
+//	}
+	
+	@GetMapping("/question/list")
+	public String list(Model model) {
+		List<Question> questionList = this.questionRepository.findAll();
+		model.addAttribute("questionList", questionList);
+		return "question_list";
+	}
+
+}
