40d88a3641e91e3014270e77b212c02dfea7087f
duck82father <legnaevol84@gmail.com>
2024-01-04 16:07:47 +0900

    08 질문 등록과 폼

--- src/main/java/kr/or/ysedu/mybo/answer/AnswerController.java
+++ src/main/java/kr/or/ysedu/mybo/answer/AnswerController.java
@@ -2,13 +2,18 @@ package kr.or.ysedu.mybo.answer;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.validation.Valid;
 import kr.or.ysedu.mybo.question.Question;
 import kr.or.ysedu.mybo.question.QuestionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RequestMapping("/answer")
 @RequiredArgsConstructor
@@ -18,11 +23,16 @@ public class AnswerController {
 	private final QuestionService questionService;
 	private final AnswerService answerService;
 	
+	@PostMapping("/create/{id}")
 	public String createAnswer(Model model, @PathVariable("id") Integer id,
-			@RequestParam(value = "content") String content) {
+			@Valid AnswerForm answerForm, BindingResult bindingResult) {
 		Question question = this.questionService.getQuestion(id);
-		this.answerService.create(question, content);
+		if (bindingResult.hasErrors()) {
+			model.addAttribute("question", question);
+			return "question_detail";
+		}
+		this.answerService.create(question, answerForm.getContent());
 		return String.format("redirect:/question/detail/%s", id);
 	}
-
+	
 }
--- src/main/java/kr/or/ysedu/mybo/question/QuestionController.java
+++ src/main/java/kr/or/ysedu/mybo/question/QuestionController.java
@@ -4,10 +4,15 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.validation.Valid;
+import kr.or.ysedu.mybo.answer.AnswerForm;
 import lombok.RequiredArgsConstructor;
 
 @RequestMapping("/question")
@@ -30,10 +35,24 @@ public class QuestionController {
 	}
 
 	@GetMapping("/detail/{id}")
-	public String detail(Model model, @PathVariable("id") Integer id) {
+	public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
 		Question question = this.questionService.getQuestion(id);
 		model.addAttribute("question",question);
 		return "question_detail";		
+	}
+	
+	@GetMapping("/create")
+	public String questionCreate(QuestionForm questionForm) {
+		return "question_form";
+	}
+	
+	@PostMapping("/create")
+	public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "question_form";
+		}
+		this.questionService.create(questionForm.getSubject(), questionForm.getContent());
+		return "redirect:/question/list";	// 질문 저장 후 질문 목록으로 이동
 	}
 	
 }
